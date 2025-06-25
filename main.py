e347aef4f3b3e60eb411435ea959d2ecf47c83ba
Tony <tony0970941055@gmail.com>
2025-06-25 16:05:33 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import pyautogui
+import pyperclip
+import time
+import os
+import platform
+import tkinter as tk
+
+msg = "這是中文自動訊息 ✨"
+count = 5
+x = 164
+y = 209
+
+def paste():
+    pyperclip.copy(msg)
+    if platform.system() == "Darwin":
+        os.system('osascript -e \'tell application "System Events" to keystroke "v" using command down\'')
+    else:
+        pyautogui.hotkey("ctrl", "v")
+
+def click_enter():
+    pyautogui.press("enter")
+
+def click_esc():
+    pyautogui.press("esc")
+
+def click_down_arrow():
+    pyautogui.press("down")
+
+def send():
+    click_enter()
+    time.sleep(0.5)
+    paste()
+    time.sleep(0.5)
+    click_enter()
+    time.sleep(0.5)
+    click_esc()
+    time.sleep(0.5)
+
+def run():
+    pyautogui.moveTo(x, y, duration=0.5)
+    pyautogui.click()
+    for i in range(count):
+        for i in range(i):
+            click_down_arrow()
+            time.sleep(0.2)
+        send()
+
+run()
+# def start_bot():
+#     message = message_entry.get()
+#     count = count_entry.get()
+#     print(f"即將發送：{message}（次數：{count}）")
+
+# # 建立主視窗
+# window = tk.Tk()
+# window.title("LINE 自動發送工具")
+# window.geometry("300x200")
+# window.configure(bg="gray15")
+
+# # 訊息輸入欄位
+# tk.Label(window, text="發送訊息：", bg="gray15", fg="white").pack(pady=(10, 0))
+# message_entry = tk.Entry(window, width=30)
+# message_entry.pack()
+
+# # 發送次數欄位
+# tk.Label(window, text="發送次數：", bg="gray15", fg="white").pack(pady=(10, 0))
+# count_entry = tk.Entry(window, width=30)
+# count_entry.pack()
+
+# # 開始按鈕
+# start_button = tk.Button(window, text="開始執行", command=start_bot)
+# start_button.pack(pady=20)
+
+# # 啟動主視窗
+# window.mainloop()
